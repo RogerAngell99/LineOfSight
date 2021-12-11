f85242905c9492758a245b17898c100b2cc10f4d
Krazune <krazune@gmail.com>
2021-12-11 17:42:58 +0000

    Fix null exception

--- src/main/java/com/krazune/lineofsight/TilesOverlay.java
+++ src/main/java/com/krazune/lineofsight/TilesOverlay.java
@@ -132,17 +132,86 @@ public class TilesOverlay extends Overlay
 		{
 			Point canvasPointA = Perspective.localToCanvas(client, new LocalPoint(localPoint.getX() - 64, localPoint.getY() + 64), plane);
 
-			int x1 = canvasPointA.getX();
-			int y1 = canvasPointA.getY();
+			if (canvasPointA != null)
+			{
+				int x1 = canvasPointA.getX();
+				int y1 = canvasPointA.getY();
 
-			Point canvasPointB = Perspective.localToCanvas(client, new LocalPoint(localPoint.getX() + 64, localPoint.getY() + 64), plane);
+				Point canvasPointB = Perspective.localToCanvas(client, new LocalPoint(localPoint.getX() + 64, localPoint.getY() + 64), plane);
 
-			int x2 = canvasPointB.getX();
-			int y2 = canvasPointB.getY();
+				if (canvasPointB != null)
+				{
+					int x2 = canvasPointB.getX();
+					int y2 = canvasPointB.getY();
 
-			graphics.drawLine(x1, y1, x2, y2);
+					graphics.drawLine(x1, y1, x2, y2);
+				}
+			}
 		}
 
+		if (rightBorder)
+		{
+			Point canvasPointA = Perspective.localToCanvas(client, new LocalPoint(localPoint.getX() + 64, localPoint.getY() - 64), plane);
+
+			if (canvasPointA != null)
+			{
+				int x1 = canvasPointA.getX();
+				int y1 = canvasPointA.getY();
+
+				Point canvasPointB = Perspective.localToCanvas(client, new LocalPoint(localPoint.getX() + 64, localPoint.getY() + 64), plane);
+
+				if (canvasPointB != null)
+				{
+					int x2 = canvasPointB.getX();
+					int y2 = canvasPointB.getY();
+
+					graphics.drawLine(x1, y1, x2, y2);
+				}
+			}
+		}
+
+		if (bottomBorder)
+		{
+			Point canvasPointA = Perspective.localToCanvas(client, new LocalPoint(localPoint.getX() - 64, localPoint.getY() - 64), plane);
+
+			if (canvasPointA != null)
+			{
+				int x1 = canvasPointA.getX();
+				int y1 = canvasPointA.getY();
+
+				Point canvasPointB = Perspective.localToCanvas(client, new LocalPoint(localPoint.getX() + 64, localPoint.getY() - 64), plane);
+
+				if (canvasPointB != null)
+				{
+					int x2 = canvasPointB.getX();
+					int y2 = canvasPointB.getY();
+
+					graphics.drawLine(x1, y1, x2, y2);
+				}
+			}
+		}
+
+		if (leftBorder)
+		{
+			Point canvasPointA = Perspective.localToCanvas(client, new LocalPoint(localPoint.getX() - 64, localPoint.getY() - 64), plane);
+
+			if (canvasPointA != null)
+			{
+				int x1 = canvasPointA.getX();
+				int y1 = canvasPointA.getY();
+
+				Point canvasPointB = Perspective.localToCanvas(client, new LocalPoint(localPoint.getX() - 64, localPoint.getY() + 64), plane);
+
+				if (canvasPointB != null)
+				{
+					int x2 = canvasPointB.getX();
+					int y2 = canvasPointB.getY();
+
+					graphics.drawLine(x1, y1, x2, y2);
+				}
+			}
+		}
+/*
 		if (rightBorder)
 		{
 			Point canvasPointA = Perspective.localToCanvas(client, new LocalPoint(localPoint.getX() + 64, localPoint.getY() - 64), plane);
@@ -187,6 +256,7 @@ public class TilesOverlay extends Overlay
 
 			graphics.drawLine(x1, y1, x2, y2);
 		}
+ */
 	}
 
 	public WorldPoint[][] getSightWorldPoints()
